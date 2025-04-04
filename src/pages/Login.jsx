7dbd9bf4f2597c612a085b3fa89e5df1e01926d8
AntoNect <antoniopiodefilippis01@gmail.com>
2025-04-04 21:37:34 +0200

    second commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { apiLogin } from "../api/BeApi";
+import { apiLogin } from "../api/api";
 import AuthContext from "../auth/AuthContext";
 import Header from "../components/Header";
 import { AlertContext } from "../context/AlertContext";
